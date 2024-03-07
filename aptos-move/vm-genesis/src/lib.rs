b54dc6d76390d91a082346e6b4cf6be7d112ce85
danielxiangzl <xiangzhuolun@gmail.com>
2024-03-06 18:55:34 -0800

    test

--- aptos-move/vm-genesis/src/lib.rs
+++ aptos-move/vm-genesis/src/lib.rs
@@ -477,13 +477,14 @@ pub fn default_features() -> Vec<FeatureFlag> {
         FeatureFlag::RESOURCE_GROUPS_SPLIT_IN_VM_CHANGE_SET,
         FeatureFlag::COMMISSION_CHANGE_DELEGATION_POOL,
         FeatureFlag::WEBAUTHN_SIGNATURE,
-        // FeatureFlag::RECONFIGURE_WITH_DKG, //TODO: re-enable once randomness is ready.
+        FeatureFlag::RECONFIGURE_WITH_DKG, //TODO: re-enable once randomness is ready.
         FeatureFlag::OIDB_SIGNATURE,
         FeatureFlag::OIDB_ZKLESS_SIGNATURE,
         // FeatureFlag::JWK_CONSENSUS,
         FeatureFlag::REFUNDABLE_BYTES,
         FeatureFlag::OBJECT_CODE_DEPLOYMENT,
         FeatureFlag::MAX_OBJECT_NESTING_CHECK,
+        FeatureFlag::FAST_RANDOMNESS,
     ]
 }
 
