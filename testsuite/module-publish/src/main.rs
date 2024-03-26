73b54bb548631bc5fc0943c31955f6019bce9d5f
Brian R. Murphy <132495859+brmataptos@users.noreply.github.com>
2024-03-26 10:48:11 -0700

    convert files that add copyright notices to files

--- testsuite/module-publish/src/main.rs
+++ testsuite/module-publish/src/main.rs
@@ -1,4 +1,4 @@
-// Copyright © Aptos Foundation
+// Copyright (c) Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
 
 use anyhow::Result;
@@ -68,7 +68,7 @@ fn main() -> Result<()> {
     //
     writeln!(
         generic_mod,
-        r#"// Copyright © Aptos Foundation
+        r#"// Copyright (c) Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0"#
     )
     .expect("Writing header comment failed");
